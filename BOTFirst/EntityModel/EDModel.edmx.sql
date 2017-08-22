c2a34a221318c2394efc6b13765a639f5471b2b7
Sergei <cryptograph2013@yandex.ru>
2017-08-22 09:48:48 +0700

    Adding entity data model as a library project with Entity Framework (#5)
    * entity model containing phrase entities was created; new database project was added to AchievementBot solution
    
    * wrong nav property and AssociationConnector were fixed
    
    * EF was removed from BOTFirst project; EntityModel library project was created and linked to BOTFirst project
    
    * names of primary keys were renamed, i.e. entity name was removed from primary key

---
+++ BOTFirst/EntityModel/EDModel.edmx.sql
@@ -0,0 +1,166 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 08/21/2017 19:26:59
+-- Generated from EDMX file: C:\!Files\!APF\!ProgrammingAndDevelopment\1Студенческий проект achievementbot\achievementbot\BOTFirst\EntityModel\EDModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [achievementbotdb];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_PhraseCanHaveMeasureUnit]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Phrases] DROP CONSTRAINT [FK_PhraseCanHaveMeasureUnit];
+GO
+IF OBJECT_ID(N'[dbo].[FK_PhraseCanHaveAction]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Phrases] DROP CONSTRAINT [FK_PhraseCanHaveAction];
+GO
+IF OBJECT_ID(N'[dbo].[FK_PhraseCanHaveAdditionalText]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Phrases] DROP CONSTRAINT [FK_PhraseCanHaveAdditionalText];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[Phrases]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Phrases];
+GO
+IF OBJECT_ID(N'[dbo].[MeasureUnits]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[MeasureUnits];
+GO
+IF OBJECT_ID(N'[dbo].[Actions]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Actions];
+GO
+IF OBJECT_ID(N'[dbo].[AdditionalTexts]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[AdditionalTexts];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Phrases'
+CREATE TABLE [dbo].[Phrases] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [OriginalMessage] nvarchar(max)  NOT NULL,
+    [CorrectedMessage] nvarchar(max)  NULL,
+    [Amount] decimal(18,0)  NULL,
+    [WasRecognized] bit  NOT NULL,
+    [MeasureUnitFId] int  NULL,
+    [ActionFId] int  NULL,
+    [AdditionalTextFId] int  NULL,
+    [Date] nchar(18)  NULL,
+    [Time] nchar(25)  NULL
+);
+GO
+
+-- Creating table 'MeasureUnits'
+CREATE TABLE [dbo].[MeasureUnits] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Text] nchar(25)  NOT NULL
+);
+GO
+
+-- Creating table 'Actions'
+CREATE TABLE [dbo].[Actions] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Text] nchar(30)  NOT NULL
+);
+GO
+
+-- Creating table 'AdditionalTexts'
+CREATE TABLE [dbo].[AdditionalTexts] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Text] nchar(50)  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Phrases'
+ALTER TABLE [dbo].[Phrases]
+ADD CONSTRAINT [PK_Phrases]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'MeasureUnits'
+ALTER TABLE [dbo].[MeasureUnits]
+ADD CONSTRAINT [PK_MeasureUnits]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Actions'
+ALTER TABLE [dbo].[Actions]
+ADD CONSTRAINT [PK_Actions]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'AdditionalTexts'
+ALTER TABLE [dbo].[AdditionalTexts]
+ADD CONSTRAINT [PK_AdditionalTexts]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [MeasureUnitFId] in table 'Phrases'
+ALTER TABLE [dbo].[Phrases]
+ADD CONSTRAINT [FK_PhraseCanHaveMeasureUnit]
+    FOREIGN KEY ([MeasureUnitFId])
+    REFERENCES [dbo].[MeasureUnits]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_PhraseCanHaveMeasureUnit'
+CREATE INDEX [IX_FK_PhraseCanHaveMeasureUnit]
+ON [dbo].[Phrases]
+    ([MeasureUnitFId]);
+GO
+
+-- Creating foreign key on [ActionFId] in table 'Phrases'
+ALTER TABLE [dbo].[Phrases]
+ADD CONSTRAINT [FK_PhraseCanHaveAction]
+    FOREIGN KEY ([ActionFId])
+    REFERENCES [dbo].[Actions]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_PhraseCanHaveAction'
+CREATE INDEX [IX_FK_PhraseCanHaveAction]
+ON [dbo].[Phrases]
+    ([ActionFId]);
+GO
+
+-- Creating foreign key on [AdditionalTextFId] in table 'Phrases'
+ALTER TABLE [dbo].[Phrases]
+ADD CONSTRAINT [FK_PhraseCanHaveAdditionalText]
+    FOREIGN KEY ([AdditionalTextFId])
+    REFERENCES [dbo].[AdditionalTexts]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_PhraseCanHaveAdditionalText'
+CREATE INDEX [IX_FK_PhraseCanHaveAdditionalText]
+ON [dbo].[Phrases]
+    ([AdditionalTextFId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
