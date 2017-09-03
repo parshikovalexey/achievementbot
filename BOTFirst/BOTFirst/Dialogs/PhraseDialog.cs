f633f23f85577e14c167728370c572b4334d46a1
Kelit <kataevdenis@mail.ru>
2017-09-04 01:43:06 +0700

    Доработка конвертации
    1) Добавлена проверка существования в БД объектов, связанных с фразой при конвертации в объект модели EF. В случае, если такие объекты уже существуют, то они будут использованы при создании фразы.
    2) Исправлена ошибка парсинга decimal, возникающая, когда во фразе нет количества.
    3) Обнаружена и исправлена ошибка "Exception: Validation failed for one or more entities. See 'EntityValidationErrors' property for more details.", причина которой была в том, была возможна ситуация, когда конвертер создавал связанные объекты Action, MeasureUnit и AdditionalText с пустыми (null) полями Text. В связи с чем валидация перед сохранением изменений в БД не проходила успешно.
    4) В модуль распознавания добавлена ссылка на EF.

--- BOTFirst/BOTFirst/Dialogs/PhraseDialog.cs
+++ BOTFirst/BOTFirst/Dialogs/PhraseDialog.cs
@@ -4,6 +4,8 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using BotPhrase;
 using EntityModel;
+using System.Diagnostics;
+using System.Data.Entity.Validation;
 
 namespace BOTFirst.Dialogs
 {
@@ -21,14 +23,34 @@ namespace BOTFirst.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
         {
             var activity = await result;
-            //var modelPhrase = Recognizer.RecognizePhrase(activity.Text);
-            //var phrase = PhrasesConverter.ModelPhraseToPhrase(modelPhrase);
 
-            //Adding data to DB example. It is necessary to implement checking of existence related objects such as actions, measureUnits and additionalTexts.
-            /*using (EDModelContainer db = new EDModelContainer()) {
-               db.Phrases.Add(modelPhrase);
-               db.SaveChanges();
-            }*/
+            //Adding data to DB example.It is necessary to implement checking of existence related objects such as actions, measureUnits and additionalTexts.
+            var phrase = Recognizer.RecognizePhrase(activity.Text);
+            var modelPhrase = PhrasesConverter.PhraseToModelPhrase(phrase);
+            Debug.Indent();
+            try
+            {
+                using (EDModelContainer db = new EDModelContainer())
+                {
+                    db.Phrases.Add(modelPhrase);
+                    db.SaveChanges();
+                }
+            }
+            catch (DbEntityValidationException e)
+            {
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Debug.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
+                            ve.PropertyName, ve.ErrorMessage);
+                    }
+                }
+                throw;
+            }
+            Debug.Unindent();
 
             await context.PostAsync(Recognizer.RecognizePhrase(activity.Text).RecognitionResult);
             await context.PostAsync("Отлично! Есть какие-либо другие новые достижения?");
--- BOTFirst/PhraseRecognizer/PhrasesConverter.cs
+++ BOTFirst/PhraseRecognizer/PhrasesConverter.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using EntityModel;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +13,9 @@ namespace BotPhrase
     {
         public static Phrase ModelPhraseToPhrase(EntityModel.Phrase modelPhrase)
         {
-              return new Phrase()
+            return new Phrase()
             {
-                Action = (modelPhrase.Action != null) ?  modelPhrase.Action.Text : "действие не указано",
+                Action = (modelPhrase.Action != null) ? modelPhrase.Action.Text : "действие не указано",
                 Amount = (modelPhrase.Amount != null) ? modelPhrase.Amount.ToString() : "количество отсутствует",
                 Units = (modelPhrase.MeasureUnit != null) ? modelPhrase.MeasureUnit.Text : "единицы измерения не указаны",
                 AdditionalText = (modelPhrase.AdditionalText != null) ? modelPhrase.AdditionalText.Text : "объект либо состояние не были указаны",
@@ -20,21 +23,61 @@ namespace BotPhrase
                 Time = (modelPhrase.Date != "") ? modelPhrase.Time : "время не было указано",
             };
         }
+
         public static EntityModel.Phrase PhraseToModelPhrase(Phrase phrase)
-         {
-             return new EntityModel.Phrase
-             {
-                 Action = new EntityModel.Action() { Text = phrase.Action },
-                 WasRecognized = phrase.WasRecognized,
-                 Date = phrase.Date,
-                 Time = phrase.Time,
-                 Amount = decimal.Parse(phrase.Amount),
-                 MeasureUnit = new EntityModel.MeasureUnit() { Text = phrase.Units },
-                 CorrectedMessage = "Коррекция правописания еще не реализована",
-                 AdditionalText = new EntityModel.AdditionalText() { Text = phrase.AdditionalText }
-             };
-         }
-        
+        {
+            var modelPhrase = new EntityModel.Phrase
+            {
+                WasRecognized = phrase.WasRecognized,
+                OriginalMessage = phrase.OriginalMessage,
+                Date = phrase.Date,
+                Time = phrase.Time,
+                CorrectedMessage = "Коррекция правописания еще не реализована",
+            };
+            using (EDModelContainer db = new EDModelContainer())
+            {
+                Debug.Indent();
+                if (db.Actions.Any() && db.Actions.Where((t) => t.Text == phrase.Action).Count() > 0)
+                {
+                    modelPhrase.ActionFId = db.Actions.Where((t) => t.Text == phrase.Action).First().Id;
+                    Debug.WriteLine("there already is such action.");
+                }
+                else
+                    modelPhrase.Action = (phrase.Action != null) ? new EntityModel.Action() { Text = phrase.Action } : null;
+
+                if (db.MeasureUnits.Any() && db.MeasureUnits.Where((t) => t.Text == phrase.Units).Count() > 0)
+                {
+                    modelPhrase.MeasureUnitFId = db.MeasureUnits.Where((t) => t.Text == phrase.Units).First().Id;
+                    Debug.WriteLine("there already is such MeasureUnit.");
+                }
+                else
+                    modelPhrase.MeasureUnit = (phrase.Units != null) ? new EntityModel.MeasureUnit() { Text = phrase.Units } : null;
+
+                if (db.AdditionalTexts.Any() && db.AdditionalTexts.Where((t) => t.Text == phrase.AdditionalText).Count() > 0)
+                {
+                    modelPhrase.AdditionalTextFId = db.AdditionalTexts.Where((t) => t.Text == phrase.AdditionalText).First().Id;
+                    Debug.WriteLine("there already is such AdditionalText."); 
+                }
+                else
+                    modelPhrase.AdditionalText = (phrase.AdditionalText != null) ? new EntityModel.AdditionalText() { Text = phrase.AdditionalText } : null;
+
+                Debug.Unindent();
+                
+
+                if (phrase.Amount != null)
+                    try
+                    {
+                        decimal.Parse(phrase.Amount);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                // TODO Implement checking of result phrase existence in DB and returning the existing phrase.
+                return modelPhrase;
+            }
+        }
 
     }
 }
+
